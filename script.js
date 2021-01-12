3b584196f34dfe3b6077af2c2332522168650fa2
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-01-12 13:04:24 +0100

    add checkErrors

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ const showError = (input, msg) => {
     const errorMsg = formBox.querySelector('.error-text');
 
     formBox.classList.add('error');
-    console.log(errorMsg);
     errorMsg.textContent = msg;
 }
 
@@ -32,10 +31,51 @@ const checkForm = input => {
         }
     })
 }
+const checkLength = (input, min) => {
+    if(input.value.length < min) {
+        showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} składa się z min. ${min} znaków.`)
+    }
+}
+
+const checkPassword = (pass1, pass2) => {
+    if (pass1.value !== pass2.value){
+        showError(pass2, 'Hasła do siebie nie pasują.')
+    }
+}
+
+const checkMail = email => {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if(re.test(email.value)) {
+        clearError(email);
+    } else {
+        showError(email, 'E-mail jest niepoprawny.');
+    }
+}
+
+const checkErrors = () => {
+    const allInputs = document.querySelectorAll('.form-box');
+    let errorCount = 0;
+
+    allInputs.forEach(el => {
+        if(el.classList.contains('error')){
+            errorCount++;
+        }
+    })
+
+    if(errorCount === 0) {
+        popup.classList.add('show-popup')
+    }
+}
 
 sendBtn.addEventListener('click', e => {
     e.preventDefault();
-    checkForm([username, pass, pass2, email])
+    checkForm([username, pass, pass2, email]);
+    checkLength(username, 3);
+    checkLength(pass, 8);
+    checkPassword(pass, pass2);
+    checkMail(email);
+    checkErrors();
 })
 
 clearBtn.addEventListener('click', e => {
@@ -43,5 +83,6 @@ clearBtn.addEventListener('click', e => {
 
     [username, pass, pass2, email].forEach(el=>{
         el.value = '';
+        clearError(el);
     })
 })
